82a25f97e864549b91859e26ffc26afcf6ecac5f
colinxg <xiao_1069@163.com>
2017-12-08 09:40:07 +0800

    modify table_99

--- table_99.py
+++ table_99.py
@@ -1,13 +1,14 @@
 # Multiplication table
-print('# version 1')
+# version 1
 
 for i in range(1, 10):
 	for j in range(1, i+1):
 		print(str(j)+'*'+str(i)+'='+str(i*j),end='  ')
 	print()
 
+
 print()
-print('# version 2')
+# version 2
 
 for i in range(1, 10):
 	for j in range(1, i+1):
@@ -16,4 +17,23 @@ for i in range(1, 10):
 			product = str(product) + ' '
 		print(str(j)+'*'+str(i)+'='+str(product), end=' ' )
 	print()
+
+
+print()
+# version 3
+
+for i in range(1, 10):
+	for j in range(1, i+1):
+		print(str(j)+'*'+str(i)+'='+str(i*j) + '\t',end=' ')
+	print()
+
+
+print()
+# version 4
+
+for i in range(1,10):
+	line = ''
+	for j in range(1, i+1):
+		line += '{}*{}={:<3}'.format(j, i, i*j)
+	print(line)
 
